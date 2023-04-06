eb142982fe8a34e64005d2475b09f954af294f90
Raj Kousik <rajkousik20@gmail.com>
2023-04-06 16:03:22 +0530

    Changes made

--- api/index.js
+++ api/index.js
@@ -14,6 +14,7 @@ import multer from "multer";
 import cookieParser from "cookie-parser";
 
 //middlewares
+
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", true);
   next();
